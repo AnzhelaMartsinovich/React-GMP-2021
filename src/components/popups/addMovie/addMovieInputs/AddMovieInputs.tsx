5171c01e2190e40e2501dac99c1aac594af43841
Anzhela Martsinovich <anzhela.martsinovich@gmail.com>
2021-04-11 23:47:14 +0300

    AddMovieInputs

--- src/components/popups/addMovie/addMovieInputs/AddMovieInputs.tsx
+++ src/components/popups/addMovie/addMovieInputs/AddMovieInputs.tsx
@@ -1,21 +1,98 @@
 import React, { FC } from 'react';
+import { connect } from 'react-redux';
 
-import { PlaceholderData } from 'baseTypes/BaseTypes.interface';
+import * as actions from 'store/actions/actions';
+import { AppState } from 'baseTypes/BaseTypes.interface';
+import { AddMovieInputsComponentProps } from './AddMovieInputs.interface';
+import {
+  onChangeTitleHandler,
+  onChangeDateHandler,
+  onChangeUrlHandler,
+  onChangeOverviewHandler,
+  onChangeRuntimeHandler,
+} from './utils/AddMovieInputsUtils';
+import { addFormPlaceholderData } from 'utils/addMovieData';
 
 import { Label } from 'components/common/label/Label.style';
 import { AddMovieInputItem, AddMovieInput } from './AddMovieInputs.style';
 
-export const AddMovieInputs: FC<PlaceholderData> = ({
-  addFormPlaceholderData,
+export const AddMovieInputsComponent: FC<AddMovieInputsComponentProps> = ({
+  setFormTitle,
+  setFormDate,
+  setFormUrl,
+  setFormOverview,
+  setFormRuntime,
 }) => (
   <>
-    {addFormPlaceholderData.map(({ id, title, placeholder, type }) => (
-      <AddMovieInputItem key={id}>
-        <Label>
-          {title}
-          <AddMovieInput placeholder={placeholder} type={type} />
-        </Label>
-      </AddMovieInputItem>
-    ))}
+    {/* {addFormPlaceholderData.map(
+        ({ id, title, placeholder, type, actionEvent }) => (
+          <AddMovieInputItem key={id}>
+            <Label>
+              {title}
+              <AddMovieInput
+                placeholder={placeholder}
+                type={type}
+                onChange={actionEvent}
+              />
+            </Label>
+          </AddMovieInputItem>
+        )
+      )} */}
+    <AddMovieInputItem key={addFormPlaceholderData[0].id}>
+      <Label>
+        {addFormPlaceholderData[0].title}
+        <AddMovieInput
+          placeholder={addFormPlaceholderData[0].placeholder}
+          type={addFormPlaceholderData[0].type}
+          onChange={(e) => onChangeTitleHandler(e, setFormTitle)}
+        />
+      </Label>
+    </AddMovieInputItem>
+    <AddMovieInputItem key={addFormPlaceholderData[1].id}>
+      <Label>
+        {addFormPlaceholderData[1].title}
+        <AddMovieInput
+          placeholder={addFormPlaceholderData[1].placeholder}
+          type={addFormPlaceholderData[1].type}
+          onChange={(e) => onChangeDateHandler(e, setFormDate)}
+        />
+      </Label>
+    </AddMovieInputItem>
+    <AddMovieInputItem key={addFormPlaceholderData[2].id}>
+      <Label>
+        {addFormPlaceholderData[2].title}
+        <AddMovieInput
+          placeholder={addFormPlaceholderData[2].placeholder}
+          type={addFormPlaceholderData[2].type}
+          onChange={(e) => onChangeUrlHandler(e, setFormUrl)}
+        />
+      </Label>
+    </AddMovieInputItem>
+    <AddMovieInputItem key={addFormPlaceholderData[3].id}>
+      <Label>
+        {addFormPlaceholderData[3].title}
+        <AddMovieInput
+          placeholder={addFormPlaceholderData[3].placeholder}
+          type={addFormPlaceholderData[3].type}
+          onChange={(e) => onChangeOverviewHandler(e, setFormOverview)}
+        />
+      </Label>
+    </AddMovieInputItem>
+    <AddMovieInputItem key={addFormPlaceholderData[4].id}>
+      <Label>
+        {addFormPlaceholderData[4].title}
+        <AddMovieInput
+          placeholder={addFormPlaceholderData[4].placeholder}
+          type={addFormPlaceholderData[4].type}
+          onChange={(e) => onChangeRuntimeHandler(e, setFormRuntime)}
+          // onChange={addFormPlaceholderData[4].actionEvent}
+        />
+      </Label>
+    </AddMovieInputItem>
   </>
 );
+
+export const AddMovieInputs = connect(
+  (state: AppState) => ({}),
+  actions
+)(AddMovieInputsComponent);
