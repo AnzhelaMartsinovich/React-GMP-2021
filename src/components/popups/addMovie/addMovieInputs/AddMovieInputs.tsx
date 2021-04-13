6ef0cda561e5a44df3fbf499c7f1cfe88646c304
Anzhela Martsinovich <anzhela.martsinovich@gmail.com>
2021-04-13 23:16:56 +0300

    Refactor AddMovieInput

--- src/components/popups/addMovie/addMovieInputs/AddMovieInputs.tsx
+++ src/components/popups/addMovie/addMovieInputs/AddMovieInputs.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from 'react';
-import { ErrorMessage } from 'formik';
 
 import { addFormPlaceholderData } from 'utils/addMovieData';
 
@@ -9,64 +8,14 @@ import { ErrorMessageStyled } from 'components/common/errorMessage/ErrorMessage.
 
 export const AddMovieInputs: FC = () => (
   <>
-    <AddMovieInputItem key={addFormPlaceholderData[0].id}>
-      <Label>
-        {addFormPlaceholderData[0].title}
-        <AddMovieInput
-          name='title'
-          placeholder={addFormPlaceholderData[0].placeholder}
-          type={addFormPlaceholderData[0].type}
-        />
-      </Label>
-      {/* <ErrorMessage name='title' component='div' /> */}
-      <ErrorMessageStyled name='title' component='div' />
-    </AddMovieInputItem>
-    <AddMovieInputItem key={addFormPlaceholderData[1].id}>
-      <Label>
-        {addFormPlaceholderData[1].title}
-        <AddMovieInput
-          name='release_date'
-          placeholder={addFormPlaceholderData[1].placeholder}
-          type={addFormPlaceholderData[1].type}
-        />
-      </Label>
-      <ErrorMessageStyled name='release_date' component='div' />
-    </AddMovieInputItem>
-    <AddMovieInputItem key={addFormPlaceholderData[2].id}>
-      <Label>
-        {addFormPlaceholderData[2].title}
-        <AddMovieInput
-          name='poster_path'
-          placeholder={addFormPlaceholderData[2].placeholder}
-          type={addFormPlaceholderData[2].type}
-        />
-      </Label>
-      {/* <ErrorMessage name='poster_path' component='div' /> */}
-      <ErrorMessageStyled name='poster_path' component='div' />
-    </AddMovieInputItem>
-    <AddMovieInputItem key={addFormPlaceholderData[3].id}>
-      <Label>
-        {addFormPlaceholderData[3].title}
-        <AddMovieInput
-          name='overview'
-          placeholder={addFormPlaceholderData[3].placeholder}
-          type={addFormPlaceholderData[3].type}
-        />
-      </Label>
-      {/* <ErrorMessage name='overview' component='div' /> */}
-      <ErrorMessageStyled name='overview' component='div' />
-    </AddMovieInputItem>
-    <AddMovieInputItem key={addFormPlaceholderData[4].id}>
-      <Label>
-        {addFormPlaceholderData[4].title}
-        <AddMovieInput
-          name='runtime'
-          placeholder={addFormPlaceholderData[4].placeholder}
-          type={addFormPlaceholderData[4].type}
-        />
-      </Label>
-      {/* <ErrorMessage name='runtime' component='div' /> */}
-      <ErrorMessageStyled name='runtime' component='div' />
-    </AddMovieInputItem>
+    {addFormPlaceholderData.map(({ id, title, placeholder, name, type }) => (
+      <AddMovieInputItem key={id}>
+        <Label>
+          {title}
+          <AddMovieInput name={name} placeholder={placeholder} type={type} />
+        </Label>
+        <ErrorMessageStyled name={name} component='div' />
+      </AddMovieInputItem>
+    ))}
   </>
 );
--- src/utils/addMovieData.ts
+++ src/utils/addMovieData.ts
@@ -3,28 +3,33 @@ export const addFormPlaceholderData = [
     title: 'Title',
     placeholder: 'Title',
     id: 1,
+    name: 'title',
   },
   {
     title: 'Release data',
     placeholder: 'Select Date',
     id: 2,
     type: 'date',
+    name: 'release_date',
   },
   {
     title: 'Movie URL',
     placeholder: 'Movie URL here',
     id: 3,
+    name: 'poster_path',
   },
   {
     title: 'Overview',
     placeholder: 'Overview here',
     id: 4,
+    name: 'overview',
   },
   {
     title: 'Runtime',
     placeholder: 'Runtime here',
     id: 5,
     type: 'number',
+    name: 'runtime',
   },
 ];
 
